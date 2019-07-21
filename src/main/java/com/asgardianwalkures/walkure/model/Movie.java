2f8952e22331ee82145cc672a5ff00fcb9f6a910
Burak Koken <burakkokenn@gmail.com>
2019-07-22 01:18:40 +0200

    Movie model has been added.

---
+++ src/main/java/com/asgardianwalkures/walkure/model/Movie.java
@@ -0,0 +1,80 @@
+package com.asgardianwalkures.walkure.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Burak Köken on 22.7.2019.
+ */
+@Entity(name = "Movie")
+public class Movie {
+
+    @Id
+    @Column(name = "movieId")
+    private Long id;
+
+    @Column(name = "movieTitle")
+    private String title;
+
+    @Column(name = "movieRuntime")
+    private int runtime;
+
+    @Column(name = "movieImdb")
+    private double imdb;
+
+    @Column(name = "movieAdult")
+    private boolean adult;
+
+    @Column(name = "movieReleaseDate")
+    @Temporal(TemporalType.DATE)
+    private Date releaseDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public double getImdb() {
+        return imdb;
+    }
+
+    public void setImdb(double imdb) {
+        this.imdb = imdb;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+}
